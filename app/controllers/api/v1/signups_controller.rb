9497af67447c732465bd8dfc9134645b00c8eff8
Zach Schneider <zach@schneid.io>
2016-03-03 15:47:37 -0600

    Fix signup parsing

--- app/controllers/api/v1/signups_controller.rb
+++ app/controllers/api/v1/signups_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class SignupsController < ApplicationController
       def create
-        signup_params = params[:signups].first.last
+        signup_params = params[:signups].values.first
         signup_params[:can_text] = (signup_params[:canText] == 'true') ? 'Yes' : 'No'
         signup_params[:event_id] ||= Event.default.id
         signup_params.reject! { |param, _| permitted_params.exclude? param.intern }
